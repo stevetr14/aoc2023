15b3e66ff52787b2445980738052c8ae294f31f7
Steven Tran <stevetr14@hotmai.com>
2023-12-04 23:10:33 +1000

    Day 4 part 2, slow af data structure

--- day_4.py
+++ day_4.py
@@ -1,5 +1,3 @@
-from collections import OrderedDict, defaultdict
-
 from utils import parse_input
 
 
@@ -18,7 +16,6 @@ def part_one():
 
         if len(x) > 0:
             points = 2 ** (len(x) - 1)
-            print(points, x)
             total += points
 
     print(total)
@@ -40,11 +37,9 @@ def count_card(
 
 
 def part_two():
-    lines = parse_input("test.txt")
+    lines = parse_input("day_4.txt")
 
-    total = 0
-    tracker_1 = defaultdict(list)
-    tracker_2 = defaultdict(int)
+    tracker = dict()
     max_length = len(lines)
 
     for i in range(max_length):
@@ -55,31 +50,38 @@ def part_two():
         winning_nums = set(winning.strip().split(" "))
         given_nums = set(right.strip().split(" "))
 
-        x = [i for i in winning_nums.intersection(given_nums) if i != ""]
+        matched_wins = [i for i in winning_nums.intersection(given_nums) if i != ""]
 
-        # tracker_1[card_num] = [card_num]
+        tracker[card_num] = []
 
-        if len(x) > 0:
-            tracker_1[card_num].extend(list(range(card_num + 1, min(card_num + len(x) + 2, max_length))))
-        else:
-            tracker_1[card_num] = []
+        num_of_win = len(matched_wins)
+        if num_of_win > 0:
+            max_card_upper = min(card_num + num_of_win + 1, max_length)
+            copies = list(range(card_num + 1, max_card_upper + 1))
+            tracker[card_num].extend(copies[:num_of_win])
 
-    for k, v in tracker_1.items():
-        # tracker_2[k] += 1
-        # print(k, v)
-        count_card(k, v, tracker_1, tracker_2)
-        # for val in v:
-        #     if val != k:
-        #         tracker_2[val] += 1
-        #         # print(tracker_1[val])
-        #         if len(tracker_1[val]) > 1:
-        #             for y in tracker_1[val]:
-        #                 tracker_2[y] += 1
+    # Generate scratch copies for each entry
+    def get_copies(mapping: dict) -> dict:
+        for k, v in mapping.items():
+            test = {item: tracker.get(item, []) for item in v}
+            mapping[k] = get_copies(test)
 
-    print(tracker_2)
-    # print(sum(tracker_2.values()))
+        return mapping
+
+    # Flatten a given dict into a list of all keys and values
+    def flatten(mapping: dict) -> list[int]:
+        result = []
+
+        for key in sorted(mapping.keys()):
+            result.append(key)
+            result.extend(flatten(mapping[key]))
+
+        return result
+
+    flattened = flatten(get_copies(tracker))
+    print(len(flattened))
 
 
 if __name__ == "__main__":
-    # part_one()
+    part_one()
     part_two()
