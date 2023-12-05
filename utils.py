cc8a06bb0803ff7842e9a3cb812bfdddf317495a
Steven Tran <stevetr14@hotmai.com>
2023-12-05 23:13:25 +1000

    Update parse_input

--- utils.py
+++ utils.py
@@ -1,3 +1,3 @@
-def parse_input(file_name: str) -> list[str]:
+def parse_input(file_name: str, split_char: str = "\n") -> list[str]:
     with open(file_name) as f:
-        return [line for line in f.read().split("\n") if line.strip() != ""]
+        return [section for section in f.read().split(split_char) if section.strip() != ""]
