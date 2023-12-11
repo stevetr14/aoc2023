7f2e6e646674a232b7f3bc83acb38b62c2bb916b
Steven Tran <steven.tran@deswik.com>
2023-12-11 15:00:14 +1000

    Day 10 - part 2 wip

--- day_10.py
+++ day_10.py
@@ -1,4 +1,5 @@
 import math
+from collections import defaultdict
 
 from utils import parse_input
 
@@ -100,10 +101,61 @@ def part_one():
 
 
 def part_two():
-    pass
+    lines = parse_input("day_10.txt")
+
+    x, y = get_starting_point(lines)
+    steps = 0
+
+    seen = []
+    is_loop_closed = False
+
+    while not is_loop_closed:
+        current_pipe = lines[y][x]
+
+        # Reset next point, any valid path should change this to be not None.
+        next_point = None
+
+        if is_top_path_valid(current_pipe, lines[y - 1][x]) and (x, y - 1) not in seen:
+            next_point = lines[y - 1][x]
+            # Go up
+            y -= 1
+            steps += 1
+        elif is_right_path_valid(current_pipe, lines[y][x + 1]) and (x + 1, y) not in seen:
+            next_point = lines[y][x + 1]
+            # Go right
+            x += 1
+            steps += 1
+        elif is_bottom_path_valid(current_pipe, lines[y + 1][x]) and (x, y + 1) not in seen:
+            next_point = lines[y + 1][x]
+            # Go down
+            y += 1
+            steps += 1
+        elif is_left_path_valid(current_pipe, lines[y][x - 1]) and (x - 1, y) not in seen:
+            next_point = lines[y][x - 1]
+            # Go left
+            x -= 1
+            steps += 1
+
+        # If there is more valid path, that means we hit the starting point "S" again. Add 1 step and end the loop.
+        if next_point is None:
+            steps += 1
+            is_loop_closed = True
+            seen.append(get_starting_point(lines))
+
+        seen.append((x, y))
+
+    for y, line in enumerate(lines):
+        row = ""
+        for x, char in enumerate(line):
+            if (x, y) in seen:
+                row += "x"
+            else:
+                row += "."
+
+        print(row)
 
 
 if __name__ == "__main__":
-    part_one()
-    # part_two()
+    # part_one()
+    part_two()
 
