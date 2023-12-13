767295de4981416fda03f70a67cd9c051cd708f0
Steven Tran <stevetr14@hotmai.com>
2023-12-13 22:36:19 +1000

    Day 13 - more wip

--- day_13.py
+++ day_13.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from pprint import pprint
 
 from utils import parse_input
 
@@ -8,34 +9,68 @@ def part_one():
 
     total = 0
 
-    for pattern in patterns:
-        row_reflection_points = [0]
-        col_reflection_points = [0]
-
+    for index, pattern in enumerate(patterns, start=1):
         lines = pattern.strip().split("\n")
         transposed_lines = ["".join(item) for item in list(zip(*lines)) if item]
 
-        for i in range(len(lines) - 1):
-            if lines[i] == lines[i + 1]:
-                row_reflection_points.append(i + 1)
+        # Check rows
+        num_of_rows = len(lines)
+        num_of_cols = len(transposed_lines)
 
-        for i in range(len(transposed_lines) - 1):
-            if transposed_lines[i] == transposed_lines[i + 1]:
-                col_reflection_points.append(i + 1)
+        a = Counter(lines)
+        b = Counter(transposed_lines)
 
-        num_of_unique_rows = len(lines) - sum([v for v in Counter(lines).values() if v > 1])
-        num_of_unique_cols = len(transposed_lines) - sum([v for v in Counter(transposed_lines).values() if v > 1])
+        print(index)
 
-        last_row_reflection = max(row_reflection_points)
-        last_col_reflection = max(col_reflection_points)
+        num_to_add = 0
 
-        if num_of_unique_rows <= num_of_unique_cols:
-            print("Row: ", last_row_reflection)
-            total += 100 * last_row_reflection
-        else:
-            print("Col: ", last_col_reflection)
-            total += last_col_reflection
+        # Check row reflection from bottom first
+        if a[lines[-1]] >= 2:
+            for i in range(num_of_rows - 1, 0, -1):
+                # Short-circuit when any non-reflection is detected in between.
+                if a[lines[i]] < 2:
+                    break
 
+                if lines[i] == lines[i - 1]:
+                    print("Row <-", i - 1)
+                    num_to_add = 100 * i
+                    break
+
+        if a[lines[0]] >= 2 and num_to_add == 0:
+            for i in range(num_of_rows - 1):
+                # Short-circuit when any non-reflection is detected in between.
+                if a[lines[i]] < 2:
+                    break
+
+                if lines[i] == lines[i + 1]:
+                    print("Row ->", i)
+                    num_to_add = 100 * (i + 1)
+                    break
+
+        # Check column reflection from right first
+        if b[transposed_lines[-1]] >= 2 and num_to_add == 0:
+            for i in range(num_of_cols - 1, 0, -1):
+                # Short-circuit when any non-reflection is detected in between.
+                if b[transposed_lines[i]] < 2:
+                    break
+
+                if transposed_lines[i] == transposed_lines[i - 1]:
+                    print("Col <-", i - 1)
+                    num_to_add = i
+                    break
+
+        if b[transposed_lines[0]] >= 2 and num_to_add == 0:
+            for i in range(num_of_cols - 1):
+                # Short-circuit when any non-reflection is detected in between.
+                if b[transposed_lines[i]] < 2:
+                    break
+
+                if transposed_lines[i] == transposed_lines[i + 1]:
+                    print("Col ->", i)
+                    num_to_add = i + 1
+                    break
+
+        total += num_to_add
         print()
 
     print(total)
