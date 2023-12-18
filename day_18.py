76a80fd5e0ea22c00ef866ae40bae7ae79c176d2
Steven Tran <stevetr14@hotmai.com>
2023-12-18 23:58:14 +1000

    Day 18 - part 2 wip

--- day_18.py
+++ day_18.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from shapely import polygons, Point
 
 from utils import parse_input
@@ -51,6 +52,89 @@ def part_one():
     print("Part 1: ", area)
 
 
+DIRECTION = {
+    "0": "R",
+    "1": "D",
+    "2": "L",
+    "3": "U",
+}
+
+
+def part_two():
+    lines = parse_input("test.txt")
+
+    points = []
+
+    x = 0
+    y = 0
+
+    _, _, first_colour = lines[0].split(" ")
+    first_colour_value = first_colour.replace("#", "").replace("(", "").replace(")", "")
+    first_steps = int(first_colour_value[:5], 16)
+    first_direction = DIRECTION[first_colour_value[-1]]
+
+    match first_direction:
+        case "R":
+            x += first_steps
+        case "L":
+            x -= first_steps
+        case "U":
+            y += first_steps
+        case "D":
+            y -= first_steps
+
+    points.append((x, y))
+
+    for line in lines[1:-1]:
+        _, _, colour = line.split(" ")
+
+        colour_value = colour.replace("#", "").replace("(", "").replace(")", "")
+        steps = int(colour_value[:5], 16)
+        direction = DIRECTION[colour_value[-1]]
+
+        match direction:
+            case "R":
+                points.append((x, y))
+                x += steps
+            case "L":
+                points.append((x, y))
+                x -= steps
+            case "U":
+                points.append((x, y))
+                y += steps
+            case "D":
+                points.append((x, y))
+                y -= steps
+
+        points.append((x, y))
+
+    # Handle last point
+    _, _, last_colour = lines[-1].split(" ")
+    last_colour_value = last_colour.replace("#", "").replace("(", "").replace(")", "")
+    last_steps = int(last_colour_value[:5], 16)
+    last_direction = DIRECTION[last_colour_value[-1]]
+
+    match last_direction:
+        case "R":
+            x += last_steps
+        case "L":
+            x -= last_steps
+        case "U":
+            y += last_steps
+        case "D":
+            y -= last_steps
+
+    points.append((x, y))
+
+    polygon = polygons(points)
+    print(polygon.area)
+    print(polygon.length)
+
+    a, b = polygon.exterior.xy
+    plt.plot(a, b)
+    plt.show()
+
+
 if __name__ == "__main__":
-    part_one()
-    # part_two()
+    # part_one()
+    part_two()
