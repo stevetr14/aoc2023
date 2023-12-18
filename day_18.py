20b85463bf4ecff3c1c1d119576669398591f486
Steven Tran <stevetr14@hotmai.com>
2023-12-18 23:58:14 +1000

    Day 18 - part 2 wip

--- day_18.py
+++ day_18.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from shapely import polygons, Point
 
 from utils import parse_input
@@ -11,35 +12,36 @@ def part_one():
     x = 0
     y = 0
 
-    all_x_values.append(x)
-    all_y_values.append(y)
-
     area = 0
 
+    points = []
+
     for line in lines:
-        direction, steps, colour = line.split(" ")
-        area += int(steps)
+        direction, steps, _ = line.split(" ")
+        steps = int(steps)
+        area += steps
 
-        for i in range(int(steps)):
-            match direction:
-                case "R":
-                    x += 1
-                case "L":
-                    x -= 1
-                case "U":
-                    y -= 1
-                case "D":
-                    y += 1
+        match direction:
+            case "R":
+                points.append((x, y))
+                x += steps
+            case "L":
+                points.append((x, y))
+                x -= steps
+            case "U":
+                points.append((x, y))
+                y += steps
+            case "D":
+                points.append((x, y))
+                y -= steps
 
-            all_x_values.append(x)
-            all_y_values.append(y)
+        all_x_values.append(x)
+        all_y_values.append(y)
 
     max_x = max(all_x_values)
     min_x = min(all_x_values)
     max_y = max(all_y_values)
     min_y = min(all_y_values)
-
-    points = [(i, j) for i, j in zip(all_x_values, all_y_values)]
 
     polygon = polygons(points)
 
@@ -49,6 +51,62 @@ def part_one():
                 area += 1
 
     print("Part 1: ", area)
+
+
+DIRECTION = {
+    "0": "R",
+    "1": "D",
+    "2": "L",
+    "3": "U",
+}
+
+
+def part_two():
+    lines = parse_input("test.txt")
+
+    points = []
+    all_x_values = []
+    all_y_values = []
+
+    x = 0
+    y = 0
+
+    area = 0
+
+    for line in lines:
+        _, _, colour = line.split(" ")
+
+        colour_value = colour.replace("#", "").replace("(", "").replace(")", "")
+        steps = int(colour_value[:5], 16)
+        direction = DIRECTION[colour_value[-1]]
+
+        area += steps
+
+        match direction:
+            case "R":
+                points.append((x, y))
+                x += steps
+            case "L":
+                points.append((x, y))
+                x -= steps
+            case "U":
+                points.append((x, y))
+                y += steps
+            case "D":
+                points.append((x, y))
+                y -= steps
+
+        points.append((x, y))
+        all_x_values.append(x)
+        all_y_values.append(y)
+
+    points.append((x, y))
+
+    polygon = polygons(points)
+    a, b = polygon.exterior.xy
+
+    plt.plot(a, b)
+    plt.show()
 
 
 if __name__ == "__main__":
